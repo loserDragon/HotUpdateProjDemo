a487ab4380b46d81d8ea00b510285fe556c6c67d
loserDragon <154413270@qq.com>
2019-02-17 23:49:35 +0800

    创建项目，并导入assetbundle框架，创建生成校验文件的编辑器脚本

---
+++ Assets/Scripts/Editor/CreateVerifyFile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using SUIFW;
+using System.Collections.Generic;
+
+public class CreateVerifyFile {
+    [MenuItem("ABTools/CreateVerifyFile")]
+    public static void GenerateVerifyFile() {
+        string m_ABOutPath = PathTools.GetPackerABPath();
+        Debug.Log("abOutPath=" + m_ABOutPath);
+        string m_VerifyFileOutPath = m_ABOutPath + "/VerifyFile.txt";
+        Debug.Log("m_VerifyFileOutPath=" + m_VerifyFileOutPath);
+        DirectoryInfo dirInfo = new DirectoryInfo(m_ABOutPath);
+        List<string> m_ListFiles = new List<string>();
+        ErgodicABOutPath(dirInfo, m_ListFiles);
+
+
+    }
+
+    private static void ErgodicABOutPath(FileSystemInfo fileSysInfo,List<string> fileList){
+        
+    }
+}
